b6c49eeb2f691bafb683880f8fc4721b4345f54e
Jerome Quere <jquere@scaleway.com>
2021-08-02 17:36:47 +0200

    feat: add support for (u)int(32/64) wrappers

--- internal/args/unmarshal.go
+++ internal/args/unmarshal.go
@@ -252,4 +252,40 @@ var unmarshalFuncs = map[protoreflect.FullName]UnmarshalFunc{
 		dest.Set(f, protoreflect.ValueOfString(value))
 		return nil
 	},
+	"google.protobuf.Int32Value": func(value string, dest protoreflect.Message) error {
+		f := dest.Descriptor().Fields().ByName("value")
+		v, err := strconv.ParseInt(value, 10, 32)
+		if err != nil {
+			return err
+		}
+		dest.Set(f, protoreflect.ValueOfInt32(int32(v)))
+		return nil
+	},
+	"google.protobuf.UInt32Value": func(value string, dest protoreflect.Message) error {
+		f := dest.Descriptor().Fields().ByName("value")
+		v, err := strconv.ParseUint(value, 10, 32)
+		if err != nil {
+			return err
+		}
+		dest.Set(f, protoreflect.ValueOfUint32(uint32(v)))
+		return nil
+	},
+	"google.protobuf.Int64Value": func(value string, dest protoreflect.Message) error {
+		f := dest.Descriptor().Fields().ByName("value")
+		v, err := strconv.ParseInt(value, 10, 64)
+		if err != nil {
+			return err
+		}
+		dest.Set(f, protoreflect.ValueOfInt64(v))
+		return nil
+	},
+	"google.protobuf.UInt64Value": func(value string, dest protoreflect.Message) error {
+		f := dest.Descriptor().Fields().ByName("value")
+		v, err := strconv.ParseUint(value, 10, 64)
+		if err != nil {
+			return err
+		}
+		dest.Set(f, protoreflect.ValueOfUint64(v))
+		return nil
+	},
 }
